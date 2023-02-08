291bdf80cb69124066d018cf41bf7a3fb34bee16
Martin Kent <119463808+martinkent2003@users.noreply.github.com>
2023-02-08 17:33:47 -0500

    Add files via upload

--- route.go
+++ route.go
@@ -5,36 +5,69 @@ import (
 	"math/rand"
 	"net/http"
 
-	"Golang-API/entity"
-	"Golang-API/repository"
+	"github.com/martinkent2003/Golang-API/entity"
+	"github.com/martinkent2003/Golang-API/repository"
+	"github.com/martinkent2003/Golang-API/groupRepository"
+
 )
 
 var (
 	repo repository.PostRepository = repository.NewPostRepository()
+	groupRepo groupRepository.GroupRepository= groupRepository.NewGroupRepository()
 )
 
-func getPosts(response http.ResponseWriter, request *http.Request) {
-	response.Header().Set("Content-Type", "application/json")
+//posts test run get and add
+func getPosts(resp http.ResponseWriter, req *http.Request) {
+	resp.Header().Set("Content-type", "application/json")
 	posts, err := repo.FindAll()
 	if err != nil {
-		response.WriteHeader(http.StatusInternalServerError)
-		response.Write([]byte(`{"error": "Error getting the posts"}`))
+		resp.WriteHeader(http.StatusInternalServerError)
+		resp.Write([]byte(`{"error": "Error gettings the posts"}`))
+		return
 	}
-	response.WriteHeader(http.StatusOK)
-	json.NewEncoder(response).Encode(posts)
+	resp.WriteHeader(http.StatusOK)
+	json.NewEncoder(resp).Encode(posts)
 }
 
-func addPost(response http.ResponseWriter, request *http.Request) {
-	response.Header().Set("Content-Type", "application/json")
+func addPost(resp http.ResponseWriter, req *http.Request) {
+	resp.Header().Set("Content-type", "application/json")
 	var post entity.Post
-	err := json.NewDecoder(request.Body).Decode(&post)
+	err := json.NewDecoder(req.Body).Decode(&post)
 	if err != nil {
-		response.WriteHeader(http.StatusInternalServerError)
-		response.Write([]byte(`{"error": "Error unmarshalling the request"}`))
+		resp.WriteHeader(http.StatusInternalServerError)
+		resp.Write([]byte(`{"error": "Error unmarshalling the posts array"}`))
 		return
 	}
 	post.ID = rand.Int63()
 	repo.Save(&post)
-	response.WriteHeader(http.StatusOK)
-	json.NewEncoder(response).Encode(post)
+	resp.WriteHeader(http.StatusOK)
+	json.NewEncoder(resp).Encode(post)
+}
+
+//group get and add
+
+func getGroups(resp http.ResponseWriter, req *http.Request) {
+	resp.Header().Set("Content-type", "application/json")
+	groups, err := groupRepo.FindAll()
+	if err != nil {
+		resp.WriteHeader(http.StatusInternalServerError)
+		resp.Write([]byte(`{"error": "Error gettings the groups"}`))
+		return
+	}
+	resp.WriteHeader(http.StatusOK)
+	json.NewEncoder(resp).Encode(groups)
+}
+
+func addGroups(resp http.ResponseWriter, req *http.Request) {
+	resp.Header().Set("Content-type", "application/json")
+	var group entity.Group
+	err := json.NewDecoder(req.Body).Decode(&group)
+	if err != nil {
+		resp.WriteHeader(http.StatusInternalServerError)
+		resp.Write([]byte(`{"error": "Error unmarshalling the groups array"}`))
+		return
+	}
+	groupRepo.Save(&group)
+	resp.WriteHeader(http.StatusOK)
+	json.NewEncoder(resp).Encode(group)
 }
--- server.go
+++ server.go
@@ -7,17 +7,19 @@ import (
 
 	"github.com/gorilla/mux"
 )
+const (
+	port string = ":8000"
+)
 
 func main() {
 	router := mux.NewRouter()
-	const port string = ":8000"
 	router.HandleFunc("/", func(response http.ResponseWriter, request *http.Request) {
-
-		fmt.Fprintln(response, "Hello World")
+		fmt.Fprintln(response, "Router is running...")
 	})
 	router.HandleFunc("/posts", getPosts).Methods("GET")
 	router.HandleFunc("/posts", addPost).Methods("POST")
-	log.Println("Listening on port", port)
+	router.HandleFunc("/groupPost", getGroups).Methods("GET")
+	router.HandleFunc("/groupPost", addGroups).Methods("POST")
+	log.Print("Server listening on port: ", port)
 	log.Fatalln(http.ListenAndServe(port, router))
-
 }
