46073c85c5ea146521f0031fec6d269ed2df6ffd
Martin Kent <119463808+martinkent2003@users.noreply.github.com>
2023-03-01 00:46:18 -0500

    Add files via upload

--- entity/group.go
+++ entity/group.go
@@ -1,6 +1,9 @@
 package entity
 
 type Group struct {
-	GroupID string   `json:"groupID"`
-	Users   []string `json:"users"`
+	GroupID     string             `json:"groupID"`
+	Users       []string           `json:"users"`
+	PlaylistID  string             `json:"playlistID"`
+	SemiMatched map[string]float64 `json:"semiMatched"`
+	Matched     map[string]float64 `json:"matched"`
 }
