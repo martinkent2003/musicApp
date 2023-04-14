f64c5e6f3608c8a415c47ea74c0423bdad9e231b
Michael Shaffer <michaelshaffer45@gmail.com>
2023-04-14 15:40:47 -0400

    redirect fixed

--- src/environment.ts
+++ src/environment.ts
@@ -1,3 +1,3 @@
 export const CLIENT_ID = '3571de52a7d747358b31518e6b0e6b1f';
 export const CLIENT_SECRET = '1dff1fc95abd4bf28c5ef114ba7e58bb';
-export const REDIRECT_URI_LOGIN = 'http://localhost:4200/login';
+export const REDIRECT_URI_LOGIN = 'http://localhost:4200/main';
